f70cb92339746ff641027761c81a73ca6fd725b9
Yeneli GM <uchyeneli@gmail.com>
2023-02-09 23:26:04 -0600

    Styiling divs and icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import '../src/styles/App.css';
+import '../src/styles/skills.css';
 
 import { MainRouter } from './routes/MainRouter';
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import me from '../assets/images/me.PNG'
 import { FiArrowUpRight } from "react-icons/fi";
+import { FaReact, FaNodeJs, FaPhp, FaGit } from "react-icons/fa";
+import { SiExpress, SiJavascript, SiCss3, SiMongodb } from "react-icons/si";
+import { AiOutlineConsoleSql, AiFillHtml5, AiFillGithub } from "react-icons/ai";
 import { Skillcard } from './SkillCard';
+import { IconContext } from "react-icons";
 
 export const Home = () => {
   return (
@@ -22,11 +26,43 @@ export const Home = () => {
         <div className='cont-image'>
           <img className='self-img' src={me} />
         </div>
-    
+
       </div>
-    <div className='cont-skills'>
-          {/* <Skillcard /> */}
+      <div className='cont-skills'>
+        {/* <Skillcard /> */}
+      </div>
+      {/* <div className='line-grad-cont'>
+        <div className='line-grad'>bar</div>
+        <div className='card-skill'>
+          
         </div>
+      </div> */}
+      <div className="parent">
+        <div className="line"></div>
+        <div className="child first">
+          <div className='skill-mask'>
+            <IconContext.Provider value={{ size: 70, color: "#3e2e5d" }}>
+              <FaReact />
+              <SiJavascript />
+              <AiFillHtml5 />
+              <SiCss3 />
+              <SiMongodb />
+              <FaNodeJs />
+              <SiExpress />
+              <AiOutlineConsoleSql />
+              <FaPhp />
+              <AiFillGithub />
+              <FaGit />
+
+            </IconContext.Provider>
+
+          </div>
+        </div>
+        <div className="child second"></div>
+        <div className="child third"></div>
+
+      </div>
+      <div>pop</div>
 
     </div>
   )
